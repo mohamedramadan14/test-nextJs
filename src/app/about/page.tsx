e4743ca6466f53858bf92ae618babac91765ea6a
mohamedramadan14 <mohameda7maa@gmail.com>
2024-11-10 09:24:45 +0200

    layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+        <h1 className="text-4xl font-bold">About</h1>
+        <p className="mt-5">ChatGPT is a chatbot powered by OpenAI&apos;s GPT-3.</p>
+        </div>
+    );
+}
+
+export default About;
